13511eb45158c9a127678216756ff8e58502bca6
Pierre Gadea <pgadea7@gmail.com>
2021-11-23 05:48:17 -0600

    Add project files.

---
+++ eShop.UseCases/PluginInterfaces/UI/IShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using eShop.CoreBusiness.Models;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.PluginInterfaces.UI
+{
+    public interface IShoppingCart
+    {
+        Task<Order> GetOrderAsync();
+        Task<Order> AddProductAsync(Product product);
+        Task<Order> UpdateQuantityAsync(int productId, int quantity);
+        Task<Order> UpdateOrderAsync(Order order);
+        Task<Order> DeleteProductAsync(int productId);
+        Task<Order> PlaceOrderAsync();
+        Task EmptyAsync();
+    }
+}
---
+++ eShop.UseCases/ViewProductScreen/ViewProductUseCase.cs
@@ -0,0 +1,21 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using eShop.UseCases.ViewProductScreen.Interfaces;
+
+namespace eShop.UseCases.ViewProductScreen
+{
+    public class ViewProductUseCase : IViewProductUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ViewProductUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public Product Execute(int id)
+        {
+            return productRepository.GetProduct(id);
+        }
+    }
+}
